0cd3eae224a9d380d721d625cc185de598ef6923
chenshuai <2851551313@qq.com>
2017-04-11 09:09:05 +0800

    提交

---
+++ app/src/main/java/com/cpigeon/app/modular/guide/view/SplashActivity.java
@@ -0,0 +1,148 @@
+package com.cpigeon.app.modular.guide.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cpigeon.app.MainActivity;
+import com.cpigeon.app.R;
+import com.cpigeon.app.commonstandard.view.activity.BaseActivity;
+import com.cpigeon.app.modular.guide.presenter.SplashPresenter;
+import com.cpigeon.app.modular.usercenter.view.activity.LoginActivity;
+import com.cpigeon.app.modular.guide.view.viewdao.ISplashView;
+import com.cpigeon.app.utils.CommonTool;
+import com.cpigeon.app.utils.NetUtils;
+import com.squareup.picasso.Picasso;
+
+import java.sql.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by chenshuai on 2017/4/10.
+ */
+
+public class SplashActivity extends BaseActivity implements ISplashView {
+
+
+    @BindView(R.id.tv_app_version)
+    TextView tvAppVersion;
+    @BindView(R.id.tv_copyright)
+    TextView tvCopyright;
+    @BindView(R.id.iv_ad_image)
+    ImageView ivAdImage;
+    @BindView(R.id.btn_ad_spik)
+    Button btnAdSpik;
+    @BindView(R.id.rl_ad)
+    RelativeLayout rlAd;
+
+    protected SplashPresenter mPresenter;
+    CountDownTimer countDownTimer;
+    boolean isEntry = false;
+
+    //最短显示时间
+    private static final long SHOW_TIME_MIN = 3400;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        mPresenter = new SplashPresenter(this);
+        //隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        tvAppVersion.setText("V " + CommonTool.getVersionName(SplashActivity.this));
+        tvCopyright.setText("中鸽科技版权所有©Copyright " + (new Date(System.currentTimeMillis()).getYear() + 1900));
+        mPresenter.welcome();
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    protected void onNetworkConnected(NetUtils.NetType type) {
+
+    }
+
+    @Override
+    protected void onNetworkDisConnected() {
+
+    }
+
+    @OnClick(R.id.btn_ad_spik)
+    public void onViewClicked() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        if (btnAdSpik != null) {
+            btnAdSpik.setText("正在进入");
+            btnAdSpik.setEnabled(false);
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                entryApp();
+            }
+        }, 1000);
+    }
+
+
+    @Override
+    public void countDown() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        countDownTimer = new CountDownTimer(SHOW_TIME_MIN, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if (btnAdSpik != null)
+                    btnAdSpik.setText(String.format("跳过(%ds)", millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                if (btnAdSpik != null)
+                    btnAdSpik.setText("跳过");
+                entryApp();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    @Override
+    public void entryApp() {
+        if (isEntry) return;
+        Intent intent;
+        if (checkLogin())
+            intent = new Intent(SplashActivity.this, MainActivity.class);
+        else
+            intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        isEntry = true;
+        finish();
+    }
+
+    @Override
+    public void showAd(String url) {
+        Picasso.with(this).load(url).into(ivAdImage);
+        rlAd.setVisibility(View.VISIBLE);
+    }
+}
--- app/src/main/java/com/cpigeon/app/modular/home/view/activity/WebActivity.java
+++ app/src/main/java/com/cpigeon/app/modular/home/view/activity/WebActivity.java
@@ -2,9 +2,19 @@ package com.cpigeon.app.modular.home.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewStub;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.cpigeon.app.R;
 import com.cpigeon.app.commonstandard.view.activity.BaseActivity;
 import com.cpigeon.app.utils.NetUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by chenshuai on 2017/4/11.
@@ -12,10 +22,24 @@ import com.cpigeon.app.utils.NetUtils;
 
 public class WebActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.pb_progressbar)
+    ProgressBar pbProgressbar;
+    @BindView(R.id.piv_tips_img)
+    ImageView pivTipsImg;
+    @BindView(R.id.tv_error_tips)
+    TextView tvErrorTips;
+    @BindView(R.id.vs_tips)
+    ViewStub vsTips;
+    @BindView(R.id.wvWebview)
+    WebView wvWebview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_web);
+        ButterKnife.bind(this);
     }
 
     @Override
